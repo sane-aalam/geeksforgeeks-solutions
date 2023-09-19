4530dc05c34eca1674b8dc6b627bf1085c803cf2
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-09-19 16:29:44 +0530

    Updated Solution - GfG to GitHub

--- Basic/Binary Search/binary-search.cpp
+++ Basic/Binary Search/binary-search.cpp
@@ -12,21 +12,22 @@ class Solution {
   public:
     int binarysearch(int arr[], int n, int key) {
         // code here
-       int start = 0;
-       int end = n - 1;
-       int mid = start + (end - start)/2;
-       
-       while(start<=end){
-           if(arr[mid] == key){
-               return mid;
-           }else if(arr[mid] < key){
-               start = mid +1;
-           }else{
-               end = mid -1;
-           }
-           mid = start + (end - start)/2;
-       }
-       return -1;
+        int start = 0;
+        int end = n - 1;
+        
+        int mid = start + (end - start)/2;
+        
+        while(start <= end){
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid] < key){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+            mid = start + (end - start)/2;
+        }
+        return -1;
     }
 };
 
